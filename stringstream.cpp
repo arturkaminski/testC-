5ebce786e6740d68a47e811f1be5a8df1b83784e
arturkaminski <muir.e@students.rutc.ac.uk>
2021-04-15 13:13:03 +0200

    Add files via upload

---
+++ stringstream.cpp
@@ -0,0 +1,30 @@
+#include <sstream>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> parseInts(string str) {
+	// Complete this function
+    stringstream ss(str);
+    vector<int> answer;
+    char ch;
+    int  n;
+    while (ss >> n) {
+        answer.push_back(n);
+        ss >> ch;
+    
+    }
+    return answer;
+}
+
+int main() {
+    string str;
+    cin >> str;
+    vector<int> integers = parseInts(str);
+    for(int i = 0; i < integers.size(); i++) {
+        cout << integers[i] << "\n";
+    }
+    
+    return 0;
+}
+
